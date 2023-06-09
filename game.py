5a4480c2cb69a4ef4b7601f1e53df1e23e514dcd
Rafzetta <83820829+Rafikka@users.noreply.github.com>
2023-06-09 17:55:18 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,74 @@
+from pygame import *
+from random import randint
+
+win_width = 1280
+win_height = 720
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+   def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed, side='left'):
+        GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y, player_speed)        
+        self.side = side
+    def update(self):
+        global side
+        if self.rect.x <= 470:
+            self.side = "right"
+        if self.rect.x >= win_width - 85:
+            self.side = "left"
+        if self.side == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed 
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Arcada")
+background = transform.scale(image.load("background.jpg"),(700, 500))
+window.blit(background,(0, 0))
+
+font.init()
+font = font.Font(None, 70)
+win = font.render('YOU WIN!', True, (255, 215, 0))
+lose = font.render('YOU LOSE!', True, (180, 0, 0))
+
+clock = time.Clock()
+FPS = 60
+game = True 
+final = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if final!=True:
+        window.blit(background,(0,0))
+        player.reset()
+
+    display.update()
+    clock.tick(FPS)
